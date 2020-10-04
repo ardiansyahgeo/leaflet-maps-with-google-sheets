6cc427874f87ec95ac150abb5f7ef965368e5a66
ardiansyahgeo <67992886+ardiansyahgeo@users.noreply.github.com>
2020-10-04 11:33:27 +0700

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -123,7 +123,7 @@ $(window).on('load', function() {
           point['Icon Color']
         );
 
-      if (point.Latitude !== '' && point.Longitude !== '') {
+      if (point.X !== '' && point.Y !== '') {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
           .bindPopup("<b>" + point['Name'] + '</b><br>' +
           (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') +
