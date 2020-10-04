c854f6011851eeacdf754477c3f1ef602660f860
ardiansyahgeo <67992886+ardiansyahgeo@users.noreply.github.com>
2020-10-04 12:22:23 +0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1IIbrIdIVjJLdSUIqS82r8Ns1HMWab6Dhhg0H7WCfPZU/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Ervf9qB7GkhRfwmPHzFG5ThMIwEHg8Snqbl57cKU1Sw/edit#gid=973618567';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
